9ad3d3a57b858d6a143b3f4b0720a50bef1f0d7e
overlasting <overlasting@620606a4-e9ba-f133-c7eb-471956230036>
2010-08-02 16:54:20 +0000

    add cc file

---
+++ trunk/src/apporo_search.cc
@@ -0,0 +1,18 @@
+#include "apporo_search.h"
+
+
+int app_vgram_match () {
+  
+}
+
+int app_ngram_match () {
+  
+}
+
+
+
+int app_surface_ngram_match(char* result_buf, int result_buf_len, const char* sufarr_file_name, int ngram_num, int result_num, const char* ranking_mode, const char* input_string, int input_string_len) {
+  
+  
+  
+}
