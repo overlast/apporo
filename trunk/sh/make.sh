220bd0f4605ba4b3a3ecd39b187650cb2b3698bb
overlasting@gmail.com <overlasting@gmail.com@620606a4-e9ba-f133-c7eb-471956230036>
2011-07-12 07:40:23 +0000

    fix filepath and filename

--- trunk/sh/make.sh
+++ trunk/sh/make.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-#g++ -c -o apporo_tsubomi_db.o ./apporo_tsubomi_db.cpp
-#g++ -c -o apporo_strdistance.o ./apporo_strdistance.cpp
-#g++ -c -o apporo_query.o ./apporo_query.cpp
-#g++ -c -o apporo_search.o ./apporo_search.cpp
-#g++ -c -o apporo_index.o ./apporo_index.cpp
-#g++ -c -o apporo_util.o ./apporo_util.cpp
-#g++ -c -o apporo_utf8.o ./apporo_utf8.cpp
+ABSDIR=$(cd $(dirname $0) && pwd)
 
+#g++ -c -o apporo_tsubomi_db.o ./apporo_tsubomi_db.cc
+#g++ -c -o apporo_strdistance.o ./apporo_strdistance.cc
+#g++ -c -o apporo_query.o ./apporo_query.cc
+#g++ -c -o apporo_search.o ./apporo_search.cc
+#g++ -c -o apporo_index.o ./apporo_index.cc
+#g++ -c -o apporo_util.o ./apporo_util.cc
+#g++ -c -o apporo_utf8.o ./apporo_utf8.cc
+
+cd ${ABSDIR}/../src
 echo "compile apporo_indexer"
-g++ -O2 -g apporo_tsubomi_db.cpp apporo_index.cpp -o apporo_indexer -ltsubomi
+g++ -O2 -g apporo_tsubomi_db.cc apporo_index.cc -o ${ABSDIR}/../bin/apporo_indexer -ltsubomi
 echo "compile apporo_searcher"
-g++ -O2 -g apporo_searcher.cpp apporo_utf8.cpp apporo_util.cpp apporo_tsubomi_db.cpp apporo_query.cpp apporo_strdistance.cpp apporo_search.cpp -o apporo_searcher -ltsubomi
-
+g++ -O2 -g apporo_searcher.cc apporo_utf8.cc apporo_util.cc apporo_tsubomi_db.cc apporo_query.cc apporo_strdistance.cc apporo_search.cc -o ${ABSDIR}/../bin/apporo_searcher -ltsubomi
