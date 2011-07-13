3798541ea16103c43e4af76205744e285b20e4b0
overlasting@gmail.com <overlasting@gmail.com@620606a4-e9ba-f133-c7eb-471956230036>
2011-07-13 07:09:32 +0000

    fix API to control the number of the entry of result list

--- trunk/src/apporo_util.cc
+++ trunk/src/apporo_util.cc
@@ -19,14 +19,16 @@ namespace apporo {
       return res;
     }
     
-    void showResult(vector < pair <double, string> > result, NgramQuery *nq, StringDistance *strdist) {
-      int result_len = result.size();
-      for (int i = 0; i < result_len; i++) {
+    void showResult(vector < pair <double, string> > result, NgramQuery *nq, StringDistance *strdist, int result_num) {
+      int result_list_len = result.size();
+      for (int i = 0; i < result_list_len; i++) {
+	if ((result_num > 0) && (i >= result_num)) { break; }
 	if ((nq != NULL) && (nq->dist_threshold > 0.0)) {
 	  if (result[i].first < nq->dist_threshold) { continue;}
 	}
 	cout << i << ":" << result[i].first << ":" << result[i].second << endl;
       }
+      return;
     }
   }
 }
