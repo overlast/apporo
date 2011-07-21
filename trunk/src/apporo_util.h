0f6d64092c3307f2b336d1c22303764788b9999b
overlasting@gmail.com <overlasting@gmail.com@620606a4-e9ba-f133-c7eb-471956230036>
2011-07-21 12:39:34 +0000

    delete obsolete code

--- trunk/src/apporo_util.h
+++ trunk/src/apporo_util.h
@@ -13,7 +13,7 @@ namespace apporo {
 
     struct pair_first_low_order {
       template <class T, class K> bool operator()(const std::pair <T, K> &x, const std::pair <T, K> &y) const {
-    return x.first > y.first;
+	return x.first > y.first;
       }
     };
 
@@ -23,47 +23,24 @@ namespace apporo {
       }
     };
 
-    
     struct pair_second_low_order {
       template <class T, class K> bool operator()(const std::pair <T, K> &x, const std::pair <T, K> &y) const {
 	return x.second > y.second;
       }
     };
+
     struct pair_second_up_order {
       template <class T, class K> bool operator()(const std::pair <T, K> &x, const std::pair <T, K> &y) const {
 	return x.second < y.second;
       }
     };
 
-    struct pair_first_pair_first_up_order {
-      template <class T, class K, class O> bool operator()(const std::pair< std::pair <T, K>, O> &x, const std::pair< std::pair <T, K>, O> &y) const {
-	return (x.first).first < (y.first).first;
-      }
-    };
-
-    struct pair_first_pair_first_low_order {
-      template <class T, class K, class O> bool operator()(const std::pair< std::pair <T, K>, O> &x, const std::pair< std::pair <T, K>, O> &y) const {
-	return (x.first).first > (y.first).first;
-      }
-    };
-    
-    struct pair_first_pair_second_up_order {
-      template <class T, class K, class O> bool operator()(const std::pair< std::pair <T, K>, O> &x, const std::pair< std::pair <T, K>, O> &y) const {
-	return (x.first).second < (y.first).second;
-      }
-    };
-    
-    struct pair_first_pair_second_low_order {
-      template <class T, class K, class O> bool operator()(const std::pair< std::pair <T, K>, O> &x, const std::pair< std::pair <T, K>, O> &y) const {
-	return (x.first).second > (y.first).second;
-      }
-    };
-    
     struct sa_range_low_order {
       template <class T, class K> bool operator()(const std::pair <T, K> &x, const std::pair <T, K> &y) const {
 	return (((x.second).second - (x.second).first) > ((y.second).second - (y.second).first));
       }
     };
+
     struct sa_range_up_order {
       template <class T, class K> bool operator()(const std::pair <T, K> &x, const std::pair <T, K> &y) const {
 	return (((x.second).second - (x.second).first) < ((y.second).second - (y.second).first));
