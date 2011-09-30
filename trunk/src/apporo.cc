659a38c31ef4f1e4b703d17d47702289cf4f4f44
overlasting@gmail.com <overlasting@gmail.com@620606a4-e9ba-f133-c7eb-471956230036>
2011-09-30 15:15:59 +0000

    fixing the function of threshold generate and fix search process

--- trunk/src/apporo.cc
+++ trunk/src/apporo.cc
@@ -68,19 +68,31 @@ void Apporo::prepare() {
   if (config->isThere("is_mecab")) { is_mecab = config->getValue<bool>("is_mecab"); }
   if (config->isThere("is_juman")) { is_juman = config->getValue<bool>("is_juman"); }
   if (config->isThere("is_kytea")) { is_kytea = config->getValue<bool>("is_kytea"); }
+
+  if (is_mecab) {
+    
+  }
   
   return;
 }
 
 vector <string> Apporo::retrieve(string &query) {
   vector <string> res;
-  NgramQuery *nq = new NgramQuery(query, ngram_length, is_pre, is_suf, is_utf8, dist_threshold);
-  StringDistance *strdist = new StringDistance(dist_func, ngram_length, nq->chars_num, dist_threshold);
+  
+  NgramQuery *nq;
+  if (is_kana) {
+    string ma_query = query;
+    nq = new NgramQuery(ma_query, ngram_length, is_pre, is_suf, is_utf8, dist_threshold);
+  }
+  else {
+    nq = new NgramQuery(query, ngram_length, is_pre, is_suf, is_utf8, dist_threshold);
+  }
+
+  StringDistance *strdist = new StringDistance(dist_func, nq->ngram_length, nq->chars_num, dist_threshold);
   NgramSearch *ngram_searcher = new NgramSearch(engine, index_path, entry_buf_len);
   vector < pair<string, sa_range> > range_vector = ngram_searcher->getRange(nq);
 
   vector <sa_index> index_vec(ngram_searcher->tdb->entry_num, 0);
-
   vector < pair <sa_index, int> > id_freq_vec = ngram_searcher->getIDMap(index_vec, range_vector, nq, strdist, bucket_size);
   vector < pair <double, string> > result = ngram_searcher->rerankAndGetResult(id_freq_vec, index_vec, nq, strdist, result_num, bucket_size);
   
